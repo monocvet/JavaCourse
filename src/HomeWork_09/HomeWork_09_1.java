6a2a36f2a9a5670c0513cf1a068c44af9c52dbd4
monocvet <monocvet@mail.ru>
2022-11-25 17:20:52 +0300

    new-1

--- src/HomeWork_09/HomeWork_09_1.java
+++ src/HomeWork_09/HomeWork_09_1.java
@@ -4,6 +4,12 @@ import java.util.ArrayList;
 
 public class HomeWork_09_1 {
     public static void main(String[] args) {
+/**  1
+ 1. Создай список строк.
+ 2. Добавь в него 5 различных строк.
+ 3. Выведи его размер на экран.
+ 4. Используя цикл выведи его содержимое на экран, каждое значение с новой строки.
+ */
         ArrayList<String> list = new ArrayList<>();
 
         list.add("Привет, мир!");
